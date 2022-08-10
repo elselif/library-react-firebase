04ae1ed715de211dd815437f09f07b112bf65f39
Elif <78614794+elselif@users.noreply.github.com>
2022-08-10 15:40:32 +0300

    route guard işlemi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route,Redirect} from 'react-router-dom'
 
 // components
 import Navbar from './components/Navbar'
@@ -6,23 +6,34 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 
+import { useAuthContext } from './hooks/useAuthContext'
+
 function App() {
+
+  const {user,authIsReady} = useAuthContext();
+
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+              <BrowserRouter>
+              <Navbar />
+              <Switch>
+                <Route exact path="/">
+                  {user && <Home />}
+                  {!user && <Redirect to="/login" />}
+                </Route>
+                <Route path="/signup">
+                  {!user && <Signup />} 
+                  {user && <Redirect to="/"/>}
+                </Route>
+                <Route path="/login">
+                {!user && <Login />} 
+                  {user && <Redirect to="/"/>}
+                </Route>
+              </Switch>
+            </BrowserRouter>
+      )}
     </div>
   );
 }
